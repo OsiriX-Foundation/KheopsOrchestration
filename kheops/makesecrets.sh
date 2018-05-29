1752ac6219fada8a0f4a035fa466f7acc1de934f
gerome <gerome@localhost.localdomain>
2018-05-29 11:49:24 +0200

    rename file

---
+++ kheops/makesecrets.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+secretsFolder=secrets
+secretsFile=(kheops_auth_hmasecret kheops_superuser_hmasecret mysql_password mysql_root_password OAuth2_secret)
+
+if [ ! -d $secretsFolder ]; then
+    mkdir $secretsFolder
+else
+    echo "$secretsFolder already exists"
+fi
+
+cd $secretsFolder
+
+for i in "${secretsFile[@]}"
+do
+    if [ ! -f $i ]; then
+        touch $i
+    else
+        echo "$i already exists"
+    fi
+done
+
+
