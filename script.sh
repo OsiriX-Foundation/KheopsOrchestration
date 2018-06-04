a8d04dfa3b8bc0323d6b26e6dd8f0fe0946a3d5e
gerome12 <gerome.pasquier@gmail.com>
2018-06-04 09:34:44 +0200

    [ci skip]

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
  sed -i "s|\"file\": {}|\"file\": \"testStudy.dcm\"|" kheops.postman_collection.json
+ sed -i "s|\"file\": {}|\"file\": \"testStudy.dcm\"|" kheops-Study_List.postman_collection.json
