5764db57e798cb179a8be1d9536c8940911a01af
gerome12 <gerome.pasquier@gmail.com>
2018-06-04 10:40:52 +0200

    ¨ci skip[

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
- sed -i "s|\"file\": {}|\"file\": \"testStudy.dcm\"|" kheops.postman_collection.json
+ sed -i "s|\"file\": {}|\"file\": \"testStudy.dcm\"|" kheops-Inbox.postman_collection.json
  sed -i "s|\"file\": {}|\"file\": \"testStudy.dcm\"|" kheops-Study_List.postman_collection.json
