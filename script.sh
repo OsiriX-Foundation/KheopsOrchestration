9bec8662f006af2374b392d3514acac48fad01a3
gerome12 <gerome.pasquier@gmail.com>
2018-06-04 14:33:27 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,3 +3,4 @@
  sed -i "s|\"file\": {}|\"file\": \"testStudy.dcm\"|" kheops-Inbox.postman_collection.json
  sed -i "s|\"file\": {}|\"file\": \"testStudy.dcm\"|" kheops-Study_List.postman_collection.json
  sed -i "s|\"file\": {}|\"file\": \"testStudy.dcm\"|" kheops-DICOMweb_Capability_Tokens.postman_collection.json
+
