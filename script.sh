de317e788e82b7c15adbe5a79180eccb81432f73
gerome12 <gerome.pasquier@gmail.com>
2018-06-15 14:21:41 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
- sed -i "s|\"file\": {}|\"file\": \"testStudy.dcm\"|" kheops-Inbox.postman_collection.json
- sed -i "s|\"file\": {}|\"file\": \"testStudy.dcm\"|" kheops-Study_List.postman_collection.json
- sed -i "s|\"file\": {}|\"file\": \"testStudy.dcm\"|" kheops-DICOMweb_Capability_Tokens.postman_collection.json
- sed -i "s|\"file\": {}|\"file\": \"testStudy.dcm\"|" kheops-proxy_pacs.postman_collection.json
+ sed -i "s|\"file\": {}|\"file\": \"testStudy.dcm\"|" postman/kheops-Inbox.postman_collection.json
+ sed -i "s|\"file\": {}|\"file\": \"testStudy.dcm\"|" postman/kheops-Study_List.postman_collection.json
+ sed -i "s|\"file\": {}|\"file\": \"testStudy.dcm\"|" postman/kheops-DICOMweb_Capability_Tokens.postman_collection.json
+ sed -i "s|\"file\": {}|\"file\": \"testStudy.dcm\"|" postman/kheops-proxy_pacs.postman_collection.json
 
