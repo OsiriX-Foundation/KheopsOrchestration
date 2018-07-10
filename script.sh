0aa9905c022337d8c8a534608db4e0e75fcc025f
gerome12 <gerome.pasquier@gmail.com>
2018-07-10 11:10:43 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,10 +6,10 @@
  sed -i "s|\"file\": {}|\"file\": \"testStudy.dcm\"|" postman/kheops-proxy_pacs.postman_collection.json
  
  
- sed -i "0,/\"file\": {}/ s/\"file\": {}/\"file\": \"testStudy.dcm\"/' postman/kheops-Study_List-withOptions.postman_collection.json
- sed -i "0,/\"file\": {}/ s/\"file\": {}/\"file\": \"testFelix.dcm\"/' postman/kheops-Study_List-withOptions.postman_collection.json
- sed -i "0,/\"file\": {}/ s/\"file\": {}/\"file\": \"testIncisix.dcm\"/' postman/kheops-Study_List-withOptions.postman_collection.json
- sed -i "0,/\"file\": {}/ s/\"file\": {}/\"file\": \"testMaceosix.dcm\"/' postman/kheops-Study_List-withOptions.postman_collection.json
- sed -i "0,/\"file\": {}/ s/\"file\": {}/\"file\": \"testMagix.dcm\"/' postman/kheops-Study_List-withOptions.postman_collection.json
+ sed -i "0,/\"file\": {}/ s/\"file\": {}/\"file\": \"testStudy.dcm\"/" postman/kheops-Study_List-withOptions.postman_collection.json
+ sed -i "0,/\"file\": {}/ s/\"file\": {}/\"file\": \"testFelix.dcm\"/" postman/kheops-Study_List-withOptions.postman_collection.json
+ sed -i "0,/\"file\": {}/ s/\"file\": {}/\"file\": \"testIncisix.dcm\"/" postman/kheops-Study_List-withOptions.postman_collection.json
+ sed -i "0,/\"file\": {}/ s/\"file\": {}/\"file\": \"testMaceosix.dcm\"/" postman/kheops-Study_List-withOptions.postman_collection.json
+ sed -i "0,/\"file\": {}/ s/\"file\": {}/\"file\": \"testMagix.dcm\"/" postman/kheops-Study_List-withOptions.postman_collection.json
  
  cat postman/kheops-Study_List-withOptions.postman_collection.json
