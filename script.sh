b1a8c1b690a2d695eca43ae5357929a78e2d3be7
gerome12 <gerome.pasquier@gmail.com>
2018-11-07 11:39:54 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
- sed -i "s|{\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-Inbox.json
- sed -i "s|{\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-Study_List.json
- sed -i "s|{\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-DICOMweb_Capability_Tokens.json
- sed -i "s|{\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-proxy_pacs.json
- sed -i "s|{\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-Zipper.json
- sed -i "s|{\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-Album.json
- sed -i "s|{\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-DICOMweb_proxy.json
- sed -i "s|{\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-Capabilities_tokens_albums.json
- sed -i "s|{\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops_Capabilities_tokens_Delete.json
- 
- cat postman/kheops-Album.json
+
+ sed -i "s|\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-Inbox.json
+ sed -i "s|\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-Study_List.json
+ sed -i "s|\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-DICOMweb_Capability_Tokens.json
+ sed -i "s|\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-proxy_pacs.json
+ sed -i "s|\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-Zipper.json
+ sed -i "s|\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-Album.json
+ sed -i "s|\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-DICOMweb_proxy.json
+ sed -i "s|\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-Capabilities_tokens_albums.json
+ sed -i "s|\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops_Capabilities_tokens_Delete.json
  
 
 
