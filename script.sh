c259ca7d75a3a01418dae2323a255d6c280daa4e
gerome12 <gerome.pasquier@gmail.com>
2019-01-08 16:15:31 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,6 +10,7 @@
  sed -i "s|\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-DICOMweb_proxy.json
  sed -i "s|\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-Capabilities_tokens_albums.json
  sed -i "s|\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops_Capabilities_tokens_Delete.json
+ sed -i "s|\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-Study_comments.json
  
 
 
