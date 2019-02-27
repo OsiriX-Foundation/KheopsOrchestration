4a3d748759698995b9c37e226e42a3302a81127c
gerome12 <gerome.pasquier@gmail.com>
2019-02-27 12:07:08 +0100

    [ci skip]

--- script.sh
+++ script.sh
@@ -12,6 +12,7 @@
  sed -i "s|\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-Study_comments.json
  sed -i "s|\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-Viewer_token.json
  sed -i "s|\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops_Capabilities_tokens_Delete.json
+ sed -i "s|\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-Introspect.json
 
 
  sed -i "0,/\"src\": null/ s/\"src\": null/\"src\": \"testStudy.dcm\"/" postman/kheops-Study_List-withOptions.json
