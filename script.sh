789419a4271fc6d9adf4a4c118e2fc1387449c00
gerome12 <gerome.pasquier@gmail.com>
2019-03-04 16:42:46 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,7 +6,6 @@
  sed -i "s|\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-DICOMweb_Capability_Tokens.json
  sed -i "s|\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-proxy_pacs.json
  sed -i "s|\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-Zipper.json
- sed -i "s|\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-Album.json
  sed -i "s|\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-DICOMweb_proxy.json
  sed -i "s|\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-Capabilities_tokens_albums.json
  sed -i "s|\"src\": null|\"src\": \"testStudy.dcm\"|" postman/kheops-Study_comments.json
@@ -20,6 +19,13 @@
  sed -i "0,/\"src\": null/ s/\"src\": null/\"src\": \"testIncisix.dcm\"/" postman/kheops-Study_List-withOptions.json
  sed -i "0,/\"src\": null/ s/\"src\": null/\"src\": \"testMaceosix.dcm\"/" postman/kheops-Study_List-withOptions.json
  sed -i "0,/\"src\": null/ s/\"src\": null/\"src\": \"testMagix.dcm\"/" postman/kheops-Study_List-withOptions.json
+ 
+ 
+ sed -i "0,/\"src\": null/ s/\"src\": null/\"src\": \"testStudy.dcm\"/" postman/kheops-Album.json
+ sed -i "0,/\"src\": null/ s/\"src\": null/\"src\": \"testFelix.dcm\"/" postman/kheops-Album.json
+ sed -i "0,/\"src\": null/ s/\"src\": null/\"src\": \"testIncisix.dcm\"/" postman/kheops-Album.json
+ sed -i "0,/\"src\": null/ s/\"src\": null/\"src\": \"testMaceosix.dcm\"/" postman/kheops-Album.json
+ sed -i "0,/\"src\": null/ s/\"src\": null/\"src\": \"testMagix.dcm\"/" postman/kheops-Album.json
  
  
  sed -i "0,/\"src\": null/ s/\"src\": null/\"src\": \"testStudy.dcm\"/" postman/kheops-series_favorite.json
