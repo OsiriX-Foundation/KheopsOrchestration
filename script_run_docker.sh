6a8dbd928720c68f5b675653c108eccc6083221b
gerome12 <gerome.pasquier@gmail.com>
2019-10-21 13:20:46 +0200

    Update script_run_docker.sh

--- script_run_docker.sh
+++ script_run_docker.sh
@@ -6,3 +6,5 @@ docker-compose up -d
 cd ..
 sleep 45
 docker ps -a
+
+node_modules/.bin/newman run postman/kheops-Get_tokens.json -e postman/kheops_localhost.postman_environment.json -g postman/Workspace.postman_globals.json --export-globals postman/Workspace.postman_globals.json
