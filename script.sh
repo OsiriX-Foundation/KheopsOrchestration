74a379c964090946103d3921fb56bec56eff53c6
gerome12 <gerome.pasquier@gmail.com>
2019-11-05 09:23:09 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -12,6 +12,8 @@
  sed -i "s|\"file\": {}|\"file\": {\"src\": \"testStudy.dcm\"}|" postman/kheops-Viewer_token.json
  sed -i "s|\"file\": {}|\"file\": {\"src\": \"testStudy.dcm\"}|" postman/kheops_Capabilities_tokens_Delete.json
  sed -i "s|\"file\": {}|\"file\": {\"src\": \"testStudy.dcm\"}|" postman/kheops-Introspect.json
+ 
+ echo postman/kheops_Capabilities_tokens_Delete.json
 
 
  sed -i "0,/\"file\": {}/s//\"file\": {\"src\": \"testStudy.dcm\"}/" postman/kheops-Study_List-withOptions.json
