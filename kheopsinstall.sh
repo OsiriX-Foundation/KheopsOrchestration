aec112558a4b90699b6e6bd412f7411754b05d52
Joël Spaltenstein <spalte@naturalimage.ch>
2020-02-03 15:52:06 +0100

    more secrets work

--- kheopsinstall.sh
+++ kheopsinstall.sh
@@ -40,25 +40,16 @@ secretfiles=("kheops_auth_hmasecret" "kheops_auth_hmasecret_post" \
 
 echo "Enter the Keycloak administrator password (user: admin):"
 read KEYCLOAK_ADMIN_PASSWORD
-printf '%s' $KEYCLOAK_ADMIN_PASSWORD | tr -dc '[:print:]' > ${secretpath}keycloak_admin_password
-printf '\n' >> ${secretpath}keycloak_admin_password
+printf "%s\n" $(printf "%s" $KEYCLOAK_ADMIN_PASSWORD | tr -dc '[:print:]') > ${secretpath}keycloak_admin_password
 
 docker pull frapsoft/openssl
 docker pull andyneff/uuidgen
 for secretfile in ${secretfiles[*]}
 do
-  secret=$(docker run -it frapsoft/openssl rand -base64 32)
-  printf '%s' $secret | tr -dc '[:print:]' > ${secretpath}tmp_${secretfile}
-  sed -e "s/\r//g" ${secretpath}tmp_${secretfile} > $secretpath$secretfile
-  printf '\n' >> $secretpath$secretfile
-  rm ${secretpath}tmp_${secretfile}
+  printf "%s\n" $(docker run -it frapsoft/openssl rand -base64 32 | tr -dc '[:print:]') > $secretpath$secretfile
 done
 
-keycloakclientsecret=$(docker run -it andyneff/uuidgen)
-printf '%s' $keycloakclientsecret | tr -dc '[:print:]' > ${secretpath}tmp_kheops_keycloak_clientsecret
-sed -e "s/\r//g" ${secretpath}tmp_kheops_keycloak_clientsecret > ${secretpath}kheops_keycloak_clientsecret
-printf '\n' >> ${secretpath}kheops_keycloak_clientsecret
-rm ${secretpath}tmp_kheops_keycloak_clientsecret
+printf "%s\n" $(docker run -it andyneff/uuidgen | tr -dc '[:print:]') > ${secretpath}kheops_keycloak_clientsecret
 
 docker rm $(docker ps -a -q)
 docker rmi frapsoft/openssl
