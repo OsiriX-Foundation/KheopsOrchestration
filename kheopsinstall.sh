64f356e47b46daa8752b2e5bb5bd1d4204c6622e
gerome12 <gerome.pasquier@gmail.com>
2021-03-02 11:05:37 +0100

    Update kheopsinstall.sh

--- kheopsinstall.sh
+++ kheopsinstall.sh
@@ -14,7 +14,7 @@ then
   exit 1
 fi
 
-download_branch="insecure-install-kibana-v0.9.6"
+download_branch="insecure-install-kibana"
 downloadURI="https://raw.githubusercontent.com/OsiriX-Foundation/KheopsOrchestration/$download_branch"
 
 secretpath="kheops/secrets"
