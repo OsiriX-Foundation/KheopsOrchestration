fdf8c4a1acd2bd2a9f9e9bffde5d1b16a8b43160
Nick Papoutsis <admin@nils.gr>
2022-07-07 16:16:05 +0200

    v1.1

--- kheopsinstall.sh
+++ kheopsinstall.sh
@@ -14,7 +14,7 @@ then
   exit 1
 fi
 
-download_branch="insecure-install-v1.0.9"
+download_branch="insecure-install-v1.1"
 downloadURI="https://raw.githubusercontent.com/OsiriX-Foundation/KheopsOrchestration/$download_branch"
 
 secretpath="kheops/secrets"
